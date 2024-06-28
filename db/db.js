d6fbd026916ef2e0c1c57c6fabe8dd70ccf45315
nikamamol <amolspatil018@gmail.com>
2024-06-28 11:38:26 +0530

    add contact form company name

--- db/db.js
+++ db/db.js
@@ -20,6 +20,7 @@ const contactFormSchema = new mongoose.Schema({
     email: { type: String, required: true },
     message: { type: String, required: true },
     phone: { type: String, required: true },
+    companyName: { type: String, required: true },
     date: { type: Date, default: Date.now }
 });
 
